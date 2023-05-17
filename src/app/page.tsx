bad82e6d6c036262c60fdabf42d68837b662333e
Mateus Almeida <mateus.belchior.98@gmail.com>
2023-05-16 22:18:37 -0300

    Criação do Conteúdo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
 export default function Home() {
-  return (
-    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
-      <h1 className="text-4xl font-bold">Sua Cápsula do Tempo</h1>
+  return <main className="grid grid-cols-2 min-h-screen">
+    {/* Left */}
+    <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
+      {/* Blur */}
+      <div className="absolute right-0 top-1/2 h-[288px] w-[526px] bg-purple-700 opacity-50 -translate-y-1/2 translate-x-1/2 rounded-full blur-full"/>
+      {/* Stripes */}
+      <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes "/>
+
+      <a href="" className="flex items-center gap-3 text-left">
+        <div className="flex h-10 w-10 items-center justify-center rounded-full"></div>
+      </a>
     </div>
-  )
+
+    {/* Right */}
+    <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
+      <div className="flex flex-1 items-center justify-center">
+        <p className="text-center leading-relaxed w-[360px]">
+          Você ainda não registrou nenhuma lembrança, comece a{' '}
+          <a href="" className="underline hover:text-gray-50">criar agora</a>
+        </p>
+      </div>
+    </div>
+  </main>
 }
