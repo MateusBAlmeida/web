a385b737074d8e331f97bde8d4c7946cac8d05f8
Mateus Almeida <mateus.belchior.98@gmail.com>
2023-05-18 13:56:41 -0300

    Aula 03 II

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { cookies } from 'next/headers'
 import { Copyright } from '@/components/Copyright'
 import { Hero } from '@/components/Hero'
 import { SignIn } from '@/components/SignIn'
+import { Profile } from '@/components/Profile'
 
 export default function Home() {
+  const isAuthenticated = cookies().has('token')
   return (
     <main className="grid min-h-screen grid-cols-2">
       {/* Left */}
@@ -13,7 +16,7 @@ export default function Home() {
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes " />
 
         {/* Sign in */}
-        <SignIn />
+        {isAuthenticated ? <Profile /> : <SignIn />}
 
         {/* Hero */}
         <Hero />
